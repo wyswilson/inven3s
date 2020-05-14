93b0e46bc212700d3c037d04857dfec170db9102
Wilson Wong <wilson.wong@go1.com>
2020-05-14 17:30:06 +1000

    Improve inventory item addition

--- webapp/src/inventory.js
+++ webapp/src/inventory.js
@@ -1,9 +1,9 @@
-import React, {Fragment} from "react";
+import React from "react";
 import "./index.css";
 import axios from 'axios';
 import { getToken } from './utils/common';
 import _ from 'lodash'
-import { Container, Grid, Dropdown, Modal, Button, Icon, Input, Label, Card, Image, Header  } from 'semantic-ui-react'
+import { Message, Container, Grid, Dropdown, Modal, Button, Icon, Input, Label, Card, Image  } from 'semantic-ui-react'
 import { DateInput } from 'semantic-ui-calendar-react';
 
 class Inventory extends React.Component {
@@ -30,33 +30,29 @@ class Inventory extends React.Component {
       tempretailername: '',
       tempquantity: 1,
       tempdateexpiry: '',
-      modalopen: false
+      modalopen: false,
+      itemaddedmsg: '',
+      itemadded: false
     }
   }
 
   fetchinventory(){
-    const { token } = this.state;
-    
-    if (!token) {
-      return;
-    }
-
     axios.get('http://127.0.0.1:8989/inventory?isedible=2&ispartiallyconsumed=2&sortby=productname',
       {
         headers: {
           "content-type": "application/json",
-          "access-token": token
+          "access-token": this.state.token
         }
       }
     )
     .then(response => { 
       if(response.status === 200){
-        const inventoryitems = response.data[0]['results'];
-        this.setState({ inventory:inventoryitems });
+        this.setState({ inventory: response.data[0]['results'] });
       }
     })
     .catch(error => {
-      if(error.response.status === 412 || error.response.status === 404){
+      const errstatus = error.response.status;
+      if(errstatus === 412 || errstatus === 404){
         console.log(error.response.data[0]['message']);
       }
       else{
@@ -70,7 +66,6 @@ class Inventory extends React.Component {
   }
 
   lookupproduct(event, data){
-    const { token } = this.state;
     const gtinorproduct = data.searchQuery
 
     if(gtinorproduct.length > 3){
@@ -78,18 +73,18 @@ class Inventory extends React.Component {
         {
           headers: {
             "content-type": "application/json",
-            "access-token": token
+            "access-token": this.state.token
           }
         }
       )
       .then(response => { 
         if(response.status === 200){
-          const suggestions = response.data[0]['results'];
-          this.updateproductsuggests(suggestions);
+          this.updateproductsuggests(response.data[0]['results']);
         }
       })
       .catch(error => {
-        if(error.response.status === 412 || error.response.status === 404){
+        const errstatus = error.response.status;
+        if(errstatus === 412 || errstatus === 404){
           console.log(error.response.data[0]['message']);
         }
         else{
@@ -100,7 +95,6 @@ class Inventory extends React.Component {
   }
 
   lookupretailer(event, data){
-    const { token } = this.state;
     const retailer = data.searchQuery
 
     if(retailer.length > 3){
@@ -108,18 +102,18 @@ class Inventory extends React.Component {
         {
           headers: {
             "content-type": "application/json",
-            "access-token": token
+            "access-token": this.state.token
           }
         }
       )
       .then(response => { 
         if(response.status === 200){
-          const suggestions = response.data[0]['results'];
-          this.updateretailersuggests(suggestions);
+          this.updateretailersuggests(response.data[0]['results']);
         }
       })
       .catch(error => {
-        if(error.response.status === 404){
+        const errstatus = error.response.status;
+        if(errstatus === 404){
           console.log(error.response.data[0]['message']);
         }
         else{
@@ -130,37 +124,27 @@ class Inventory extends React.Component {
   }
 
   lookupbrand(brand){
-    const { token } = this.state;
-
     if(brand.length > 3){
       axios.get('http://127.0.0.1:8989/brand/' + brand,
         {
           headers: {
             "content-type": "application/json",
-            "access-token": token
+            "access-token": this.state.token
           }
         }
       )
       .then(response => { 
         if(response.status === 200){
-          const suggestions = response.data[0]['results'];
-          this.setState({ brandsuggests:suggestions });
-          console.log(suggestions);
+          this.setState({ brandsuggests: response.data[0]['results'] });
         }
       })
       .catch(error => {
-        const errresponse = error.response
-        if(errresponse){
-          if(errresponse.status === 412){
-            const message = errresponse.data[0]['message'];
-            console.log(message);
-          }
-          else{
-            console.log(errresponse);
-          }
+        const errstatus = error.response.status;
+        if(errstatus === 412){
+          console.log(error.response.data[0]['message']);
         }
         else{
-          console.log('internal server error');
+          console.log('fatal: server unreachable');
         }
       });
     }
@@ -189,8 +173,6 @@ class Inventory extends React.Component {
   }
   
   addinventory(event){
-    const { token } = this.state;
-
     const { tempgtin, tempproductname, tempretailername, tempquantity, tempdateexpiry} = this.state;
   
     console.log("saving=>" + tempgtin + ':' + tempproductname + ':' + tempretailername + ':' + tempquantity + ':' + tempdateexpiry);
@@ -208,19 +190,30 @@ class Inventory extends React.Component {
         headers: {
           'crossDomain': true,
           "content-type": "application/json",
-          "access-token": token
+          "access-token": this.state.token
         }
       }
     )
     .then(response => { 
       if(response.status === 200){
-        console.log(response);
-        this.closemodal();      
+        this.setState({ itemaddedmsg: response.data[0]['message'] });
+        this.setState({ itemadded: true });
+        this.setState({ gtin: '' });
+        this.setState({ retailername: ''});
+        this.setState({ dateexpiry: ''});
+        this.setState({ quantity: 1 });
       }
     })
     .catch(error => {
-      const errresponse = error.response;
-      console.log(errresponse);
+      const errstatus = error.response.status;
+      if(errstatus === 412 || errstatus === 404 || errstatus === 403 || errstatus === 503){
+        console.log(error.response);
+        this.setState({ itemaddedmsg: error.response.data[0]['message'] });        
+      }
+      else{
+        console.log('fatal: server unreachable');
+        this.setState({ itemaddedmsg: 'fatal: server unreachable' });
+      }
     });
   }
 
@@ -256,12 +249,119 @@ class Inventory extends React.Component {
     console.log(data.value);
   }
 
+  inventoryremove(){
+    console.log("hi");  
+  }
+  
   openmodal = () => this.setState({ modalopen: true })
-
   closemodal = () => this.setState({ modalopen: false })
 
-  render() {
-    const griditems = _.map(this.state.inventory, (item) => (
+  generategriddefault(){
+    let itemaddedstatus = '';
+    if(this.state.itemaddedmsg !== ''){
+      if(this.state.itemadded){
+        itemaddedstatus = (
+          <Message className="fullwidth" success><Message.Header>{this.state.itemaddedmsg}</Message.Header></Message>
+        );
+      }
+      else{
+        itemaddedstatus = (
+          <Message className="fullwidth" negative><Message.Header>{this.state.itemaddedmsg}</Message.Header></Message>        
+        );
+      }
+    }
+    return (
+      <Card raised key="1">
+        <Card.Content>
+          <Image rounded
+            centered src="https://react.semantic-ui.com/images/wireframe/image.png"
+            floated='right'
+            size='tiny'
+          />
+          <Card.Header size='tiny'>Add new item</Card.Header>
+          <Card.Meta></Card.Meta>
+          <Label color='grey' attached='top right'>0</Label>
+        </Card.Content>
+
+        <Card.Content extra textAlign="center">
+          <Modal
+            trigger={<Button icon="plus" onClick={this.openmodal} />}
+            open={this.state.modalopen}
+            onClose={this.closemodal} 
+            centered={false}
+            basic
+            size="fullscreen"
+            dimmer="blurring"
+            >
+            <Modal.Header>add item into inventory</Modal.Header>
+            <Modal.Content>
+              <Grid columns={1} container doubling stackable>
+                <Grid.Column>
+                  <Dropdown className="fullwidth"
+                    placeholder="product"
+                    search
+                    selection
+                    allowAdditions
+                    value={this.state.tempproductname}
+                    options={this.state.productsuggests}
+                    additionLabel = "add new product "
+                    onSearchChange={this.lookupproduct.bind(this)}
+                    onAddItem={this.addnewproduct.bind(this)}
+                    onChange={this.setinventorymetadata.bind(this)}
+                  />
+                </Grid.Column>
+                <Grid.Column>
+                  <Dropdown className="fullwidth"
+                    placeholder="retailer"
+                    search
+                    selection
+                    allowAdditions
+                    value={this.state.tempretailername}
+                    options={this.state.retailersuggests}
+                    additionLabel = "add new retailer "
+                    onSearchChange={this.lookupretailer.bind(this)}
+                    onAddItem={this.addnewretailer.bind(this)}
+                    onChange={this.setinventorymetadata.bind(this)}
+                  />
+                </Grid.Column>
+                <Grid.Row columns={2}>
+                  <Grid.Column>
+                    <Input className="fullwidth"
+                      placeholder='quantity'
+                      value={this.state.tempquantity}
+                      onChange={this.setinventorymetadata.bind(this)}
+                    />
+                  </Grid.Column>
+                  <Grid.Column>
+                     <DateInput className="fullwidth"
+                      placeholder="expiry"
+                      dateFormat="YYYY-MM-DD"
+                      value={this.state.tempdateexpiry}
+                      onChange={this.setinventorymetadata.bind(this)}
+                    />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Modal.Content>
+            <Modal.Actions>
+              <Grid columns={2} container doubling stackable>
+                <Grid.Column>
+                  <Button className="fullwidth" color='black' onClick={this.addinventory.bind(this)}>
+                    <Icon name='checkmark' />save
+                  </Button>
+                </Grid.Column>
+                <Grid.Column>
+                  {itemaddedstatus}
+                </Grid.Column>
+              </Grid>
+            </Modal.Actions>
+          </Modal>
+        </Card.Content>
+      </Card>
+    )
+  }
+  generategriditems(){
+    return _.map(this.state.inventory, (item) => (
             <Card raised key={item.gtin}>
               <Card.Content>
                 <Image rounded
@@ -297,100 +397,20 @@ class Inventory extends React.Component {
                       </Modal.Description>
                     </Modal.Content>
                   </Modal>
-                  <Button icon="minus" />
+                  <Button icon="minus" onClick={this.inventoryremove}/>
                   <Button icon="plus" />
                 </div>
               </Card.Content>
             </Card>
           ))
+  }
 
+  render() {
     return (
       <Container fluid>
-         
         <Card.Group doubling itemsPerRow={6} stackable>
-          <Card raised key="1">
-            <Card.Content>
-              <Image rounded
-                centered src="https://react.semantic-ui.com/images/wireframe/image.png"
-                floated='right'
-                size='tiny'
-              />
-              <Card.Header size='tiny'>Add new item</Card.Header>
-              <Card.Meta></Card.Meta>
-              <Label color='grey' attached='top right'>0</Label>
-            </Card.Content>
-
-            <Card.Content extra textAlign="center">
-              <Modal
-                trigger={<Button icon="plus" onClick={this.openmodal} />}
-                open={this.state.modalopen}
-                onClose={this.closemodal} 
-                centered={false}
-                basic
-                size="fullscreen"
-                dimmer="blurring"
-                >
-                <Modal.Header>add item into inventory</Modal.Header>
-                <Modal.Content>
-                  <Grid columns={1} container doubling stackable>
-                    <Grid.Column>
-                      <Dropdown className="fullwidth"
-                        placeholder="product"
-                        search
-                        selection
-                        allowAdditions
-                        value={this.state.tempproductname}
-                        options={this.state.productsuggests}
-                        additionLabel = "add new product "
-                        onSearchChange={this.lookupproduct.bind(this)}
-                        onAddItem={this.addnewproduct.bind(this)}
-                        onChange={this.setinventorymetadata.bind(this)}
-                      />
-                    </Grid.Column>
-                    <Grid.Column>
-                      <Dropdown className="fullwidth"
-                        placeholder="retailer"
-                        search
-                        selection
-                        allowAdditions
-                        value={this.state.tempretailername}
-                        options={this.state.retailersuggests}
-                        additionLabel = "add new retailer "
-                        onSearchChange={this.lookupretailer.bind(this)}
-                        onAddItem={this.addnewretailer.bind(this)}
-                        onChange={this.setinventorymetadata.bind(this)}
-                      />
-                    </Grid.Column>
-                    <Grid.Row columns={2}>
-                      <Grid.Column>
-                        <Input className="fullwidth"
-                          placeholder='quantity'
-                          value={this.state.tempquantity}
-                          onChange={this.setinventorymetadata.bind(this)}
-                        />
-                      </Grid.Column>
-                      <Grid.Column>
-                         <DateInput className="fullwidth"
-                          placeholder="expiry"
-                          dateFormat="YYYY-MM-DD"
-                          value={this.state.tempdateexpiry}
-                          onChange={this.setinventorymetadata.bind(this)}
-                        />
-                      </Grid.Column>
-                    </Grid.Row>
-                  </Grid>
-                </Modal.Content>
-                <Modal.Actions>
-                  <Button color='black' onClick={this.addinventory.bind(this)}>
-                    <Icon name='checkmark' />save
-                  </Button>
-                </Modal.Actions>
-              </Modal>
-            </Card.Content>
-          </Card>
-
-          {griditems}
-
+          {this.generategriddefault()}
+          {this.generategriditems()}
         </Card.Group>
       </Container>
     )
