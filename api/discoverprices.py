08e356c4e307e5b80abf8cca842da60a70ac316a
Wilson Wong <wyswilson@live.com>
2020-10-06 17:57:42 +1100

    Laying foundation for price scraping

--- api/discoverprices.py
+++ api/discoverprices.py
@@ -48,167 +48,170 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-def pricescraper_coles(gtin,productname):
-	price = 0
-	source = "Coles"
-	status = ""
 
-	url = "https://api.coles.com.au/customer/v1/coles/products/search?limit=3&q=%s&storeId=7716&type=SKU" % (productname)
-	header = {
-	'Accept-Encoding': 'gzip'
-	,'Connection': 'keep-alive'
-	,'Accept': '*/*' 
-	,'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-	,'X-Coles-API-Key': '046bc0d4-3854-481f-80dc-85f9e846503d'
-	,'X-Coles-API-Secret': 'e6ab96ff-453b-45ba-a2be-ae8d7c12cadf'
-	,'Accept-Language': 'en-AU;q=1'
-	}#FROM https://github.com/adambadge/coles-scraper/blob/master/coles.ipynb
+def pricescrape(url):
+	price = '0.0'
+	retailer = ''
+	matchobj = ""
 
-	response = requests.get(url, headers=header)
-	status = response.status_code
+	html,urlresolved = func.fetchhtml(url)
+	soup = bs4.BeautifulSoup(html, 'html.parser')
 
-	if status == 200:
-		json = response.json()
-		results = json['Results']
-		if results:
-			promo = results[0]['Promotions']
-			if promo:
-				price = promo[0]['Price']
+	if re.search(r'amcal\.com\.au',url): 
+		retailer = 'amcal'
+		matchobj = soup.find_all('span',{'class':'price'})
+
+	elif re.search(r'discountchemist\.com\.au',url): 
+		retailer = 'discountchemist'
+		matchobj = soup.find_all('span',{'class':'woocommerce-Price-amount amount'})
+
+	elif re.search(r'igashop\.com\.au',url): 
+		retailer = 'iga'
+		matchobj = soup.find_all('span',{'class':'woocommerce-Price-amount amount'})
+
+	elif re.search(r'bigw\.com\.au',url): 
+		retailer = 'bigw'
+		matchobj = soup.find_all('span',{'id':'product_online_price'})
+
+	elif re.search(r'woolworths\.com\.au',url): 
+		retailer = 'woolworths'
+		matchobj = soup.find_all('div',{'class':'price price--large'})
+	
+	elif re.search(r'coles\.com\.au',url): 
+		retailer = 'coles'
+		matchobj = soup.find_all('span',{'class':'price-container'})
+
+	elif re.search(r'asiangrocerystore\.com\.au',url): 
+		retailer = 'asiangrocerystore'
+		matchobj = soup.find_all('span',{'id':'line_discounted_price_126'})
+
+	elif re.search(r'drakes\.com\.au',url): 
+		retailer = 'drakes'
+		matchobj = soup.find_all('strong',{'class':'MoreInfo__Price'})
+
+	elif re.search(r'mysweeties\.com\.au',url): 
+		retailer = 'mysweeties'
+		matchobj = soup.find_all('span',{'id':'productPrice'})
+
+	elif re.search(r'allysbasket\.com',url): 
+		retailer = 'allysbasket'
+		matchobj = soup.find_all('span',{'itemprop':'price'})
+
+	elif re.search(r'buyasianfood\.com\.au',url): 
+		retailer = 'buyasianfood'
+		matchobj = soup.find_all('span',{'class':'price'})
+
+	elif re.search(r'chemistwarehouse\.com\.au',url): 
+		retailer = 'chemistwarehouse'
+		matchobj = soup.find_all('span',{'class':'product__price'})
+
+	elif re.search(r'goodpricepharmacy\.com\.au',url): 
+		retailer = 'goodpricepharmacy'
+		matchobj = soup.find_all('span',{'class':'price'})
+		
+	elif re.search(r'indoasiangroceries\.com\.au',url): 
+		retailer = 'indoasiangroceries'
+		matchobj = soup.find_all('p',{'class':'price'})
+
+	elif re.search(r'myasiangrocer\.com\.au',url): 
+		retailer = 'myasiangrocer'
+		matchobj = soup.find_all('span',{'class':'price'})
+
+	elif re.search(r'pharmacydirect\.com\.au',url): 
+		retailer = 'pharmacydirect'
+		matchobj = soup.find_all('span',{'id':'price-display'})
+
+	elif re.search(r'officeworks\.com\.au',url): 
+		retailer = 'officeworks'
+		matchobj = re.findall('"edlpPrice":"(.+?)"', html, re.IGNORECASE)
+
+
+	if matchobj and retailer != '':
+		for match in matchobj:
+			if type(match) == str:
+				price = match
 			else:
-				status = 204
-		else:
-			status = 204
-	else:
-		errstr = "coles error: [%s] [%s]" % (status,response.reason)
+				price = match.text
+
+			price = price.replace('\n' , '') 
+			price = price.replace('Price:' , '') 
+			price = price.replace('$' , '') 
+			try:
+				test = float(price)
+			except:
+				price = "0.0"
+			break
+	elif retailer == '':
+		retailer = 'not-supported'
+		errstr = "price-scraper-norules: [%s] [%s]" % (retailer,url)
 		print(errstr)
-		logging.debug(errstr)	
-
-	return price,source,status
-
-def pricescraper_bigw(gtin,productname):
-	price = 0
-	source = "Big W"
-	status = ""
-
-	searchurl = "https://www.bigw.com.au/search/?text=" + gtin
-	randagent = random.choice(useragents)
-	headers = {'User-Agent': randagent}
-	response = requests.get(searchurl, headers=headers)
-	status = response.status_code
-
-	if status == 200:
-		html = response.content.decode('utf-8')	
-		datamatch = re.findall('products_storage = (\[.+\]);', html, re.IGNORECASE)
-		for data in datamatch:
-			if data:
-				dataobj = json.loads(data)
-				price = dataobj[0]['price']
-			else:
-				status = 204
 	else:
-		errstr = "bigw error: [%s] [%s]" % (status,response.reason)
+		errstr = "price-scraper-err: [%s] [%s]" % (retailer,url)
 		print(errstr)
 		logging.debug(errstr)
 
-	return price,source,status
+	return float(price),retailer
 
-def pricescraper_woolworths(gtin,productname):
-	price = 0
-	source = "Woolworths"
-	status = ""
+#url = "https://shop.coles.com.au/a/richmond-south/product/claratyne-childrens-grape-chew-5mg-tabs-10pk"
 
-	searchurl = "https://www.woolworths.com.au/apis/ui/Search/products"
-	randagent = random.choice(useragents)
-	headers = {'User-Agent': randagent}
-	payload = {
-		'Location': "/shop/search/products?searchTerm=" + productname,
-		'PageNumber': 1, 
-		'PageSize': 24, 
-		'SearchTerm': productname,
-		'SortType': "TraderRelevance"
-	}
-	response = requests.post(searchurl, headers=headers, data=payload)
-	status = response.status_code
-
-	if status == 200:
-		respjson 	= json.loads(response.text)
-		items 		= respjson['Products']
-		if items:
-			for item in items:
-				itemobj = item['Products'][0]
-				price_ 	= itemobj['Price']
-				barcode = itemobj['Barcode']
-				
-				if gtin == barcode:
-					price = price_
-					status = 200
-					break
-				else:
-					status = 204
-		else:
-			status = 204
-	else:
-		errstr = "woolworths error: [%s] [%s]" % (status,response.reason)
-		print(errstr)
-		logging.debug(errstr)
-
-	return price,source,status
-
-#url = "https://shop.coles.com.au/a/national/product/uncle-tobys-golden-syrup-quick-sachet-oats"
-#url = "https://www.uncletobys.com.au/products/oats/uncle-tobys-quick-sachets/quick-sachets-golden-syrup"
-#url = "https://www.woolworths.com.au/shop/productdetails/368716/uncle-tobys-oats-quick-sachets-classics-variety-porridge"
-#url = "https://www.woolworths.com.au/shop/productdetails/724514/cocobella-cocobella-straight-up-coconut-water"
+#url = "https://igashop.com.au/product/leggos-pesto-traditional-basil-190g/"
+#url = "https://www.amcal.com.au/soov-bite-25g-p-93327381"
+#url = "https://www.bigw.com.au/product/oral-b-stages-2-mickey-2-4-years-toothbrush-extra-soft/p/169614/"
+#url = "https://discountchemist.com.au/product/ego-pinetarsol-solution-100ml/"
+#url = "https://www.woolworths.com.au/shop/productdetails/94375/leggo-s-pesto-traditional-basil"
+#url = "http://www.asiangrocerystore.com.au/lee-kum-kee-char-siu-sauce-397g.html"
+#url = "https://062.drakes.com.au/lines/obento-seasoning-sushi-250ml"
+#url = "https://mysweeties.com.au/products/doritos-salsa-dip-mild-300g-1-unit"
+#url = "https://www.allysbasket.com/biscuits-crackers/10608-arnott-s-premier-cookies-chocolate-chip-310g.html"
+#url = "https://www.buyasianfood.com.au/_products/NoodlesPasta/JapaneseKoreanNoodles/HakubakuOrganicUdonNoodles270G-25-35-.aspx"
+#url = "https://www.chemistwarehouse.com.au/buy/89956/grants-of-australia-toothpaste-propolis-with-mint-110g-online-only"
+#url = "https://www.goodpricepharmacy.com.au/palmolive-foam-hand-wash-antibacterial-lime-250ml"
+#url = "https://www.indoasiangroceries.com.au/lee-kum-kee-premium-dark-soy-sauce-500-ml"
+#url = "https://www.myasiangrocer.com.au/ajishima-nori-komi-furikake-rice-seasoning-50g/"
+#url = "https://www.officeworks.com.au/shop/officeworks/p/glen-20-disinfectant-300g-lavender-le0357053"
+#url = "https://www.pharmacydirect.com.au/dermeze-treatment-cream-500g"
 
 query1 = """
 	SELECT 
 		p.gtin,
 		p.productname,
 		DATE_FORMAT(CONVERT_TZ(NOW(),'+00:00','+10:00'), "%Y-%m-%d"),
-		GROUP_CONCAT(distinct CONCAT(pp.retailer,':',pp.price) SEPARATOR ';') AS retailers,		
+		GROUP_CONCAT(distinct pc.candidateurl ORDER BY pc.candidaterank ASC SEPARATOR '; ') as priceurls,
 		COUNT(*) 
 	FROM products AS p
 	LEFT JOIN inventories AS i
 	ON p.gtin = i.gtin
 	LEFT JOIN productsprice AS pp
 	ON p.gtin = pp.gtin AND pp.date = DATE_FORMAT(CONVERT_TZ(NOW(),'+00:00','+10:00'), "%Y-%m-%d")
+	LEFT JOIN productscandidate AS pc
+	ON p.gtin = pc.gtin AND pc.`type` = 'productprice'
 	GROUP BY 1,2,3
-	ORDER BY 5 DESC
+	ORDER BY 5 DESC, 1 ASC
 """
 cursor.execute(query1)
 records = cursor.fetchall()
+
 for record in records:
-	gtin 		= record[0]
-	productname	= record[1]
-	retailers    = record[3]
-	if not retailers:
-		retailers = ""
-	timestamp 	= datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-	date 		= datetime.datetime.today().strftime('%Y-%m-%d')
+	gtin 			= record[0]
+	productname		= record[1]
+	date         	= record[2]
+	urls         	= record[3]
 
-	print("[%s] [%s] [%s] [%s]" % (date,gtin,productname,retailers))
-	
-	if not re.search(r'Woolworths',retailers): 
-		price,source,status = pricescraper_woolworths(gtin,productname)
-		print("\t[%s] [%s] [%s]" % (source,str(price),status))
-		if float(price) > 0:
+	processedretailers = {}
+	for url in urls.split("; "):
+		price,retailer = pricescrape(url)
+		print("[%s][%s]" % (retailer,price))
+		if float(price) > 0 and not any(retailer in key for key in processedretailers):
+			timestamp 	= datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+			date 		= datetime.datetime.today().strftime('%Y-%m-%d')
 			query1 = "REPLACE INTO productsprice (gtin,price,timestamp,date,retailer) VALUES (%s,%s,%s,%s,%s)"
-			cursor.execute(query1,(gtin,price,timestamp,date,source))
+			cursor.execute(query1,(gtin,price,timestamp,date,retailer))
 			db.commit()	
+		elif float(price) == 0:
+			print("no price detected [%s][%s]" % (retailer,price))		
+		else:
+			print("price from retailer already exists [%s][%s]" % (retailer,price))		
 
-	if not re.search(r'Big W',retailers): 
-		price,source,status = pricescraper_bigw(gtin,productname)
-		print("\t[%s] [%s] [%s]" % (source,str(price),status))
-		if float(price) > 0:
-			query1 = "REPLACE INTO productsprice (gtin,price,timestamp,date,retailer) VALUES (%s,%s,%s,%s,%s)"
-			cursor.execute(query1,(gtin,price,timestamp,date,source))
-			db.commit()	
-
-	if not re.search(r'Coles',retailers): 
-		price,source,status = pricescraper_coles(gtin,productname)
-		print("\t[%s] [%s] [%s]" % (source,str(price),status))
-		if float(price) > 0:
-			query1 = "REPLACE INTO productsprice (gtin,price,timestamp,date,retailer) VALUES (%s,%s,%s,%s,%s)"
-			cursor.execute(query1,(gtin,price,timestamp,date,source))
-			db.commit()	
+		processedretailers[retailer] = ''
 
 	time.sleep(5)
